2cf852e5bf2e6aab8505740bd357b79e8d87be69
KimTaeO <taeo0328@gmail.com>
2023-07-26 10:44:25 +0900

    :sparkles: :: Board ViewSet 작성

--- crud/board/views.py
+++ crud/board/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import BoardSerializer
+from .models import Board
 
-# Create your views here.
+class BoardViewSet(viewsets.ModelViewSet):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
