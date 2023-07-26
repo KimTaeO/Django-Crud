c0d2fddf2b28e1fb159fabbd77747a305da75bfc
KimTaeO <taeo0328@gmail.com>
2023-07-26 09:29:58 +0900

    :sparkles: :: mysql에 모델 반영

--- crud/board/models.py
+++ crud/board/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField("제목", max_length=20, null=False)
+    content = models.TextField("내용", null=False)
